e87c65e4bb6a383e966ad7d1b28284296e03a18e
AjSantiago <isc_aj.santiago@hotmail.com>
2022-12-13 22:22:45 -0600

    SessionStorage Hook

---
+++ src/hooks/useSessionStorage.ts
@@ -0,0 +1,11 @@
+import React from 'react';
+
+export const useSessionStorage = (key: string): any | boolean => {
+  const storedValue = sessionStorage.getItem(key);
+
+  if (!storedValue) {
+    return false;
+  } else {
+    return storedValue;
+  }
+};
