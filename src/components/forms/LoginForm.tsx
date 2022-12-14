80c290ac8c63f37e5ef9e962aafc37fa90bfd00d
AjSantiago <isc_aj.santiago@hotmail.com>
2022-12-13 22:00:51 -0600

    Login Form ready

--- src/components/forms/LoginForm.tsx
+++ src/components/forms/LoginForm.tsx
@@ -1,6 +1,7 @@
-import React from 'react'
+import React from 'react';
+import { useNavigate } from 'react-router-dom';
 
-import { Formik, Form, Field, ErrorMessage } from 'formik'
+import { Formik, Form, Field, ErrorMessage } from 'formik';
 import * as Yup from 'yup';
 
 import { login } from '../../services/authService';
@@ -8,74 +9,97 @@ import { AxiosResponse } from 'axios';
 
 //* Define Schema of validation with Yup
 const loginSchema = Yup.object().shape({
-    email: Yup.string().email('Invalid email format').required('Email is required'),
-    password: Yup.string().required('Password is required')
-})
+  email: Yup.string()
+    .email('Invalid email format')
+    .required('Email is required'),
+  password: Yup.string().required('Password is required'),
+});
 
 //* Login Component
 const LoginForm = () => {
-    const initialCredentials = {
-        email: '',
-        password:''
-    }
+  const initialCredentials = {
+    email: '',
+    password: '',
+  };
 
-    return (
-        <div>
-            <h4>Login Form</h4>
-            {/* Formik to encapsulate a Form */}
-            <Formik
-                initialValues={initialCredentials}
-                validationSchema={loginSchema}
-                onSubmit={async (values) => {
-                    login(values.email, values.password).then((response: AxiosResponse) => {
-                        if (response.status === 200) {
-                            if (response.data.token) {
-                                sessionStorage.setItem('sessionJWTToken', response.data.token)
-                            } else {
-                            throw new Error('Error generating Login Token')
-                            }
-                        } else {
-                            throw new Error('Invalid credentials')
-                        }
-                    }).catch((error)=>console.error(`[Login Error]: Something went wrong ${error}`))
-                }}
-            >
-                {
-                    ({ values, touched, errors, handleBlur, handleChange, isSubmitting }) => (
-                        <Form>
-                            {/* Email Field */}
-                            <label htmlFor='email'>Email</label>
-                            <Field id='email' type='email' name='email' placeholder='example@email.com' />
+  let navigate = useNavigate();
 
-                            {/* Email Errors */}
-                            {
-                                errors.email && touched.email && (
-                                    <ErrorMessage name='email' component='div'></ErrorMessage>
-                                )
-                            }
-
-                            {/* Password Field */}
-                            <label htmlFor='password'>Password</label>
-                            <Field id='password' type='password' name='password' placeholder='example' />
-
-                            {/* Password Errors */}
-                            {
-                                errors.password && touched.password && (
-                                    <ErrorMessage name='password' component='div'></ErrorMessage>
-                                )
-                            }
-
-                            {/* Submit Form */}
-                            <button type='submit'>Login</button>
-
-                            {/* Message if the form is submitting */}
-                            {isSubmitting ? (<p>Checking credentials...</p>) : null }
-                        </Form>
-                    )
+  return (
+    <div>
+      <h4>Login Form</h4>
+      {/* Formik to encapsulate a Form */}
+      <Formik
+        initialValues={initialCredentials}
+        validationSchema={loginSchema}
+        onSubmit={async (values) => {
+          login(values.email, values.password)
+            .then(async (response: AxiosResponse) => {
+              if (response.status === 200) {
+                if (response.data.token) {
+                  await sessionStorage.setItem(
+                    'sessionJWTToken',
+                    response.data.token
+                  );
+                  navigate('/');
+                } else {
+                  throw new Error('Error generating Login Token');
                 }
-            </Formik>
-        </div>
-    )
-}
+              } else {
+                throw new Error('Invalid credentials');
+              }
+            })
+            .catch((error) =>
+              console.error(`[Login Error]: Something went wrong ${error}`)
+            );
+        }}
+      >
+        {({
+          values,
+          touched,
+          errors,
+          handleBlur,
+          handleChange,
+          isSubmitting,
+        }) => (
+          <Form>
+            {/* Email Field */}
+            <label htmlFor='email'>Email</label>
+            <Field
+              id='email'
+              type='email'
+              name='email'
+              placeholder='example@email.com'
+            />
+
+            {/* Email Errors */}
+            {errors.email && touched.email && (
+              <ErrorMessage name='email' component='div'></ErrorMessage>
+            )}
+
+            {/* Password Field */}
+            <label htmlFor='password'>Password</label>
+            <Field
+              id='password'
+              type='password'
+              name='password'
+              placeholder='Password'
+            />
+
+            {/* Password Errors */}
+            {errors.password && touched.password && (
+              <ErrorMessage name='password' component='div'></ErrorMessage>
+            )}
+
+            {/* Submit Form */}
+            <button type='submit'>Login</button>
+
+            {/* Message if the form is submitting */}
+            {isSubmitting ? <p>Checking credentials...</p> : null}
+          </Form>
+        )}
+      </Formik>
+    </div>
+  );
+};
 
 export default LoginForm;
