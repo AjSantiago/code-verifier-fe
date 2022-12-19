f8141780cef71b5350790f2a9a0d911f4ddf6cf5
AjSantiago <isc_aj.santiago@hotmail.com>
2022-12-18 18:29:13 -0600

    Kata Service & Page & DetailPage done

--- src/pages/KatasDetailPage.tsx
+++ src/pages/KatasDetailPage.tsx
@@ -1,6 +1,10 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useNavigate, useParams } from 'react-router-dom';
 import { useSessionStorage } from '../hooks/useSessionStorage';
+import { getKataById } from '../services/katasService';
+import { AxiosResponse } from 'axios';
+import { Kata } from '../utils/types/Kata.type';
+import { Editor } from '../components/editor/Editor';
 
 export const KatasDetailPage = () => {
   let loggedIn = useSessionStorage('sessionJWTToken');
@@ -9,15 +13,57 @@ export const KatasDetailPage = () => {
   //* Find id from params
   let { id } = useParams();
 
+  const [kata, setKata] = useState<Kata | undefined>(undefined);
+  const [showSolution, setShowSolution] = useState(false);
+
   useEffect(() => {
     if (!loggedIn) {
       return navigate('/login');
+    } else {
+      if (id) {
+        getKataById(loggedIn, id)
+          .then((response: AxiosResponse) => {
+            if (response.status === 200 && response.data) {
+              let kataData = {
+                _id: response.data._id,
+                name: response.data.name,
+                description: response.data.description,
+                stars: response.data.stars,
+                level: response.data.level,
+                intents: response.data.intents,
+                creator: response.data.creator,
+                solution: response.data.solution,
+                participants: response.data.participants,
+              };
+              setKata(kataData);
+              console.table(kataData);
+            }
+          })
+          .catch((error) => console.error(`[Kata by Id Error]: ${error}`));
+      } else {
+        return navigate('/katas');
+      }
     }
   }, [loggedIn]);
 
   return (
     <div>
       <h1>Katas Detail Page {id} </h1>
+      {kata ? (
+        <div className='kata-data'>
+          <h2>{kata?.description}</h2>
+          <h3>Rating: {kata.stars}/5</h3>
+
+          <button onClick={() => setShowSolution(!showSolution)}>
+            {showSolution ? 'Show Solution' : 'Hide Solution'}
+          </button>
+          {showSolution ? null : <Editor>{kata?.solution}</Editor>}
+        </div>
+      ) : (
+        <div>
+          <h2>Loading data...</h2>
+        </div>
+      )}
     </div>
   );
 };
--- src/pages/KatasPage.tsx
+++ src/pages/KatasPage.tsx
@@ -1,15 +1,40 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import { useSessionStorage } from '../hooks/useSessionStorage';
+import { getAllKatas } from '../services/katasService';
+import { AxiosResponse } from 'axios';
+import { Kata } from '../utils/types/Kata.type';
 
 export const KatasPage = () => {
   let loggedIn = useSessionStorage('sessionJWTToken');
   //* Variable to navigate between stack of routes
   let navigate = useNavigate();
+  const [katas, setKatas] = useState([]);
+  const [totalPages, setTotalPages] = useState(1);
+  const [currentPage, setCurrentPage] = useState(1);
 
   useEffect(() => {
     if (!loggedIn) {
       return navigate('/login');
+    } else {
+      getAllKatas(loggedIn, 2, 1)
+        .then((response: AxiosResponse) => {
+          if (
+            response.status === 200 &&
+            response.data.katas &&
+            response.data.totalPages &&
+            response.data.currentPage
+          ) {
+            console.table(response.data);
+            let { katas, totalPage, currentPage } = response.data;
+            setKatas(katas);
+            setTotalPages(totalPage);
+            setCurrentPage(currentPage);
+          } else {
+            throw new Error(`Error obtaining katas: ${response.data}`);
+          }
+        })
+        .catch((error) => console.error(`Get all katas error ${error}`));
     }
   }, [loggedIn]);
 
@@ -24,12 +49,25 @@ export const KatasPage = () => {
   return (
     <div>
       <h1>Katas Page</h1>
-      {/* TODO: Real katas */}
-      <ul>
-        {/* TODO: Export to isolated component */}
-        <li onClick={() => navigateToKataDetail(1)}>First kata</li>
-        <li onClick={() => navigateToKataDetail(2)}>Second kata</li>
-      </ul>
+      {katas.length > 0 ? (
+        <div>
+          {/* TODO: Export to isolated component */}
+          {katas.map((kata: Kata) => (
+            <div key={kata._id}>
+              <h3 onClick={() => navigateToKataDetail(kata._id)}>
+                {kata.name}
+              </h3>
+              <h4>{kata.description}</h4>
+              <h5>Creator: {kata.creator}</h5>
+              <h5>Rating: {kata.stars}/5</h5>
+            </div>
+          ))}
+        </div>
+      ) : (
+        <div>
+          <h5>No katas found</h5>
+        </div>
+      )}
     </div>
   );
 };
